f3f60ccd18e224fc1edb227f3203e7a10dbc2540
kamchykulmatov <kulmatovkamchy9@gmail.com>
2024-08-26 14:14:07 +0600

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Integer firstOperand;
+    private Integer secondOperand;
+    private Integer result;
+    private Button button;
+    private Boolean isOperationClick;
+    private String operation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textView = findViewById(R.id.text_view);
+        button = findViewById(R.id.btn_2);
+    }
+    public void OnNumberClick(View view) {
+        if (view.getId() == R.id.btn_clear) {
+            textView.setText("0");
+        } else {
+            String text = ((MaterialButton) view).getText().toString();
+            if (textView.getText().toString().equals("0") || isOperationClick) {
+                textView.setText(text);
+            } else {
+                textView.append(text);
+            }
+        }
+        button.setVisibility(View.GONE);
+        isOperationClick = false;
+    }
+
+    public void OnOperationClick(View view) {
+        button.setVisibility(View.GONE);
+        if (view.getId() == R.id.btn_plus) {
+            firstOperand = Integer.valueOf(textView.getText().toString());
+            operation = "+";
+
+        } else if (view.getId() == R.id.btn_division) {
+            firstOperand = Integer.valueOf(textView.getText().toString());
+            operation = "/";
+
+        } else if (view.getId() == R.id.btn_minus) {
+            firstOperand = Integer.valueOf(textView.getText().toString());
+            operation = "-";
+
+        } else if (view.getId() == R.id.btn_multiplication) {
+            firstOperand = Integer.valueOf(textView.getText().toString());
+            operation = "*";
+
+        } else if (view.getId() == R.id.btn_egual) {
+            secondOperand = Integer.valueOf(textView.getText().toString());
+            if (operation.equals("+")) {
+                result = firstOperand + secondOperand;
+                textView.setText(result.toString());
+            } else if (operation.equals("-")) {
+                result = firstOperand - secondOperand;
+                textView.setText(result.toString());
+            } else if (operation.equals("*")) {
+                result = firstOperand * secondOperand;
+                textView.setText(result.toString());
+            } else if (operation.equals("/")) {
+                if (secondOperand == 0) {
+                    textView.setText("Error");
+                } else {
+                    result = firstOperand / secondOperand;
+                }
+            }
+            textView.setText(result.toString());
+            button.setVisibility(View.VISIBLE);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("result", result);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+        }
+        isOperationClick = true;
+    }
+
+}
---
+++ app/src/main/java/com/example/calculator/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.example.calculator;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView resultView;
+    Button buttonNext;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent = getIntent();
+        int result = intent.getIntExtra("result",0);
+
+        resultView = findViewById(R.id.result_textView);
+        resultView.setText(String.valueOf(result));
+    }
+
+    public void clickNext(View view) {
+        finish();
+    }
+}
